c9cf6ed23828721120336b6275c29d504d6e63b0
kuoyu2tap <kuoyuand@gmail.com>
2017-10-22 14:01:13 +0800

    Sun Oct 22 14:01:13 CST 2017

--- commitauto.sh
+++ commitauto.sh
@@ -7,9 +7,11 @@ cd /root/gitcommit/randomfile
 if [[ $(($r%13))!=0 ]];
 then
 	i=$(($r%3))
+	echo $i
 	while [[ -z i ]];do
 		echo -e $r"`date`\n">>"$r.txt"
 		i=$(($i-1))
+		echo $i
 	done
 
 	cd ..
