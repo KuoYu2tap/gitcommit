3ceef02a41aae86d787df604343d8bc890c08307
kuoyu2tap <kuoyuand@gmail.com>
2017-10-22 13:59:38 +0800

    Sun Oct 22 13:59:38 CST 2017

--- commitauto.sh
+++ commitauto.sh
@@ -6,7 +6,12 @@ cd /root/gitcommit/randomfile
 
 if [[ $(($r%13))!=0 ]];
 then
-	echo -e $r"`date`\n">>"$r.txt"
+	i=$(($r%3))
+	while [[-z i]];do
+		echo -e $r"`date`\n">>"$r.txt"
+		i=$(($i-1))
+	done
+
 	cd ..
 	git add -A
 	git commit -m "`date`"
