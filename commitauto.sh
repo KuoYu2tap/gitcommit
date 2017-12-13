b3a04fe3ed4d791f851790f6701cacbb3c9c888a
kuoyu2tap <kuoyuand@gmail.com>
2017-12-13 19:11:08 +0800

    Wed Dec 13 19:11:08 CST 2017

--- commitauto.sh
+++ commitauto.sh
@@ -4,10 +4,9 @@ nowpwd="/root/gitcommit/randomfile/"
 
 
 while [[ $r -gt 0 ]];do
-	echo -e "`free -m`\n">> $nowpwd"`date`.txt"
+	echo -e "`free -m`\n">> $nowpwd"`date +%D_%T_%N`.txt"
 	r=$(($r-1))
 done
 
 git add -A
 git commit -m "`date`"
-
