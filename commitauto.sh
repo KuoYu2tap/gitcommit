32b549abd26b807dea9d4d5793964369fc40e635
kuoyu2tap <kuoyuand@gmail.com>
2018-04-21 13:18:01 +0800

    Sat Apr 21 13:18:01 CST 2018

--- commitauto.sh
+++ commitauto.sh
@@ -8,7 +8,7 @@ cd $nowpwd
 MSG=`python2.6 /root/show_ss_msg.py`
 echo "key: $r\n" >> /root/commit_keys
 while [[ $r -gt 0 ]];do
-	echo -e "`free -m`\n">> $nowpwd"commits/""`date +%F_%T`$MSG.txt"
+	echo -e "`free -m`\n">> $nowpwd"commits/""`date +%F_%T_%N`"_"$MSG.txt"
 	r=$(($r-1))
 done
 
